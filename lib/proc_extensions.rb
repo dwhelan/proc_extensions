6952f23b77b322ce46cfbb6211526e22b95f8298
dwhelan <dwhelan@dpwhelan.com>
2015-11-23 20:07:07 -0500

    Removed RSpec matcher

--- lib/proc_extensions.rb
+++ lib/proc_extensions.rb
@@ -1,6 +1,5 @@
 require_relative 'proc_extensions/version'
 require_relative 'proc_extensions/proc_source'
-require_relative 'proc_extensions/match'
 
 require_relative 'core_extensions/proc/inspect'
 require_relative 'core_extensions/proc/match'
